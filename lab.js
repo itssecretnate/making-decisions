7904a849a1f96d9e06f7e1c398f37b2e19796ce3
Nathan Mena <natemena153@gmail.com>
2022-02-09 16:55:24 -0700

    Fixing problem 7

--- lab.js
+++ lab.js
@@ -69,28 +69,32 @@ let rain = true
 /*
   Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, based on the above temperature and rain.
   
-  If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. 
+  
   If it is between 60 and 80 degrees, and raining, one should wear a rain-jacket. 
+  If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. 
+  
   If it is 60 degrees or below, one should wear a jacket and carry an umbrella.
   
   *Come up with your own clothing suggestions based on those temperature parameters when it is not raining.*
-*/
+  */
+ 
 
 //CODE HERE
 
 // If it is raining
-if(rain) {
+if(rain === true) {
   if(temperature >= 80) console.log("One should wear a t-shirt and take an umbrella.")
   else if(temperature > 60 && temperature < 80) console.log("One should wear a rain-jacket.");
 }
 
-// If it's not raining
-if(!rain) {
+// If it's not raining (My own clothing suggetions)
+if(rain === false) {
   if(temperature <= 32) console.log("You should wear a winter coat today. It looks like it's below freezing!");
   else if(temperature > 32 && temperature < 70) console.log("You should wear a long sleeve shirt. It's chilly!");
   else if(temperature >= 70) console.log("You should wear a t-shirt today. It appears to be warm!");
 }
 
+// We don't know if it's raining.
 else if(temperature <= 60 && temperature > 32) console.log("One should wear a jacket and carry an umbrella.");
 else console.log("I'm not sure what you should wear today...");
 
@@ -116,12 +120,12 @@ for(let i = 0; i < 10; i++) {
 
 // One way of handling it.
 for(let i = 1; i <= 10; i++) {
-  console.log(`Iteration: ${i}`);
+  console.log(i);
 }
 
 // Altnerative way.
 for(let i = 0; i < 10; i++) {
-  console.log(`Alternative Iteration: ${i+1}`);
+  console.log(i+1);
 }
 
 ////////// PROBLEM 7 //////////
@@ -132,15 +136,12 @@ for(let i = 0; i < 10; i++) {
 
 //CODE HERE
 
-// One way of handling it.
-for(let i = 10; i > 0; i--) {
+// Answer
+for(let i = 10; i >= 0; i--) {
   console.log(`Reverse Iteration: ${i}`);
 }
 
-// Altnerative way.
-for(let i = 9; i >= 0; i--) {
-  console.log(`Reverse Alternative Iteration: ${i+1}`);
-}
+
 
 let score = 0
 let passingScore = 7
